cb1798e394f8c34eb649e1f79da542155ba134a2
Kim Lew <kimlew451@gmail.com>
2021-11-27 13:51:44 -0700

    Initial commit of Python Flask app folders and files

---
+++ pythonapp-createjazzlyric/conn_vars_dict.py
@@ -0,0 +1,8 @@
+import os
+
+conn_dict = {
+    'host': os.environ.get('DB_HOST'),
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'database': os.environ.get('DB_NAME'),
+}
