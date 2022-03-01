0963400b60bf53876096cd35859ce39ceeff8f0d
Kim Lew <kimlew451@gmail.com>
2022-03-01 14:59:38 -0700

    Creates 1 main script that calls other scripts to simplify process

--- README.md
+++ README.md
@@ -4,32 +4,11 @@
 
 ## On Your Computer
 
-1. In the directory for the VM/virtual machine, where you have the Vagrantfile, start the VM & run SSH with these commands:
+1. In the directory for the VM/virtual machine/deployment machine, where you have the Vagrantfile, run this command:
 
   ```
-  vagrant up
-  vagrant ssh
+  ./main.sh
   ```
-
-2. In a second terminal, go to the same directory and create a copy of the Vagrant ssh configuration file with: `vagrant ssh-config > vagrant-ssh-config`
-
-3. Also in the second terminal, in the same directory, copy the required files to the VM, that set up the machine and are the app: `bash copy_req_files_to_vm.sh`
-
-## In a Shell on the VM/Virtual Machine/Deployment Machine
-
-1. **IMPORTANT**: You MUST create `mysql_pw.txt` on the VM before you run the next 2 Bash scripts. Create `mysql_pw.txt` at the the root directory, i.e., at the same level as the Bash setup scripts, `bash setup_machine.sh` and `bash setup_mysql.sh`. In `mysql_pw.txt`, put in a login password for the root user for MySQL on this VM.
-
-2. From the root directory, set up MySQL with run: `bash setup_mysql.sh`
-
-3. Verify MySQL has been installed & there are no error messages.
-
-4. From the root directory, set up the machine wit run: `bash setup_machine.sh`
-
-5. Verify versions shown in the Terminal output are correct. Also in the output, you should see: `✔ Successfully created virtual environment!`
-
-6. Change into the subdirectory, `pythonapp-createjazzlyric` if you are not there, e.g., `cd /home/vagrant/pythonapp-createjazzlyric`
-
-7. From there, run the app with: `pipenv run flask run`
 
 ## In a Browser Tab
 
---
+++ main.sh
@@ -0,0 +1,72 @@
+#! /usr/bin/env bash
+
+# Script name: main.sh
+
+# Description: This script runs all the other scripts required to setup the
+# machine with MySQL and other programs to be able to run the Python Flask app,
+# Create Jazz Lyric. This script uses the commands:
+# read - to read the desired database password from the user, with -s option to
+# not echo passwords characters to terminal
+# ssh - to run individual commands on the vagrant box
+# nohup - on the Vagrant box, to run 'pipenv run flask run' in the background
+# without quitting when 'ssh' disconnects
+
+# Author: Kim Lew
+
+hide_mid_chars() {
+  PASSWD="$1"
+  LEN="${#PASSWD}"
+  FIRST="${PASSWD:0:1}"
+  MID="${PASSWD:1:$((LEN-2))}"
+  LAST="${PASSWD:$((LEN-1)):1}"
+  STARS="${MID//?/*}"
+  echo -n "Password is: ${FIRST}${STARS}${LAST}"
+  echo
+}
+# Prompt user for root MySQL password, with -s so is silent mode/chars not shown,
+# e.g., read -p "Type password: " -s -r MYSQL_ROOT_PWD
+# stty -echo still prints out the characters, but hides any input that didn't make it to the read command
+# Verifies password according to MySQL password policies in setup_mysql.sh.
+# echo -n "Type a root password for MySQL: "
+read -rsep "Type a root password for MySQL: " PASSWORD_ROOT
+echo
+read -rsep "Re-type the root password for MySQL: " PASSWORD_CONFIRMED
+echo
+echo
+
+hide_mid_chars "${PASSWORD_ROOT}"
+if [ ! "${PASSWORD_ROOT}" = "${PASSWORD_CONFIRMED}" ]; then
+  echo "Passwords entered do NOT match."
+  echo -n "Confirmed "
+  hide_mid_chars "${PASSWORD_CONFIRMED}"
+  echo "Re-run main.sh."
+  exit 1
+fi
+echo
+echo "Setting up Create Jazz Lyric..."
+echo
+
+# -- Run commands these on your local computer. --
+vagrant up
+vagrant ssh-config > vagrant-ssh-config
+chmod u+x copy_req_files_to_vm.sh
+./copy_req_files_to_vm.sh
+
+#  -- Use ssh to run commands in shell on VM/Virtual Machine/Deployment Machine. --
+# Use vagrant ssh - for every command on VM, e.g., ssh -F vagrant-ssh-config, etc.
+
+# ssh -F vagrant-ssh-config default -- copy_proj_to_vm.sh
+ssh -F vagrant-ssh-config default -- chmod u+x setup_mysql.sh
+ssh -F vagrant-ssh-config default -- chmod u+x setup_machine.sh
+
+#  ssh -F vagrant-ssh-config default -- cat stuff.txt
+ssh -F vagrant-ssh-config default -- cat \> mysql_pw.txt < <(echo "$PASSWORD_ROOT")  # bash named pipe, man process substitution redirection?
+ssh -F vagrant-ssh-config default -- ./setup_mysql.sh
+ssh -F vagrant-ssh-config default -- ./setup_machine.sh
+
+# TRICKY Part: These 2 lines must be run on VM, but within this script, you are NOT on the VM.
+# Change into the subdirectory, `pythonapp-createjazzlyric` if you are not there.
+ssh -F vagrant-ssh-config default -- cd /home/vagrant/pythonapp-createjazzlyric \&\& pipenv run flask run
+
+# In a Browser Tab: Check that the app is running at: <http://localhost:8084/>
+# Note: The ports were forwarded in the `Vagrantfile` to port `8084`.
